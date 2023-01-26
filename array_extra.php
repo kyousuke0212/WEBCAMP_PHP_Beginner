b28f3732078a6be649b125c8209a2b25fedd2e8b
tomioka kyousuke <tomioka kyousuke@example.com>
2023-01-26 12:19:18 +0000

    array_extra.phpを書いた

---
+++ array_extra.php
@@ -0,0 +1,27 @@
+<?php
+
+$array_1 = [
+    0 => '1st',
+    1 => '2st',
+    2 => '3st',
+    ];
+    
+    var_dump($array_1);
+    
+    $array_2 = [
+    2 => '3rd',
+    0 => '1st',
+    -100 => -100,
+    1 => '2nd',
+];
+var_dump($array_2);
+
+$array_3 = [
+    "key1" => "Hello",
+    2 => '3rd',
+    0 => '1st',
+    -100 => -100,
+    "key2" => 3.14,
+    "key3" => 999,
+];
+var_dump($array_3);
